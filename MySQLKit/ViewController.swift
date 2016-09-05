6ba994adc7920b263990e51402dc8c8fbc1c6ea2
Tanmay Bakshi <tajymany@gmail.com>
2016-09-05 12:41:19 -0400

    Update ViewController.swift

--- MySQLKit/ViewController.swift
+++ MySQLKit/ViewController.swift
@@ -9,17 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    let msk_php_url = "http://www.tanmaybakshi.com/getanything.php"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let databaseGet = "products_available"
-        let MSK = MySQLKit(MSKHandler: msk_php_url, database: databaseGet)
-        //MSK.runRequest_DATA(query: "INSERT INTO icecream (flavor, quantity) VALUES ('chocolate',5);")
-        //MSK.runRequest_DATA(query: "INSERT INTO icecream (flavor, quantity) VALUES ('strawberry',3);")
-        //MSK.runRequest_DATA(query: "SELECT * FROM icecream").asString.printSelf()
     }
     
     override func didReceiveMemoryWarning() {
